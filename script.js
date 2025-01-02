347c074cc7db46f36bba87cadc7979b4ef5ff419
fredsimoes78 <fredsimoes78@gmail.com>
2025-01-02 18:54:34 +0000

    trying to put time difference

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function updateClock() {
+    const due_date = new Date('2034-11-10')
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    const diff = due_date - now  //this is miliseconds
+
+    const days = String(diff.getDays()).padStart(4, '0');
+    const hours = String(diff.getHours()).padStart(2, '0');
+    const mins = String(diff.getMinutes()).padStart(2, '0');
+    const secs = String(diff.getSeconds()).padStart(2, '0');
+
+    document.getElementById('clock').textContent = `${days}${hours}:${mins}:${secs}`;
   }
   
   // Update the clock every second
-  setInterval(updateClock, 1000);
+  setInterval(updateClock, 1000); //every 1000 miliseconds
   
   // Initialize the clock immediately
   updateClock();
